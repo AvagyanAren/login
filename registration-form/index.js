aa6302e10609d329807c05f951cbbffdd0c88734
Aren <avagyanarenn@gmail.com>
2023-04-27 12:29:46 +0400

    Complete login form design

---
+++ registration-form/index.js
@@ -0,0 +1,13 @@
+// var input = document.querySelector("#phone");
+// window.intlTelInput(input, {
+//   separateDialCode: true
+// });
+
+
+// let body = document.getElementsByTagName("BODY")[0];
+// body.style.backgroundImage = "url(C:\Users\Aren\Desktop\Hproject\registration-form\images\background.jpg)";
+
+document.body.style.backgroundImage = "url(C:\Users\Aren\Desktop\Hproject\registration-form\images\background.jpg)";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundPosition = "center";
+document.body.style.backgroundRepeat = "no-repeat";
